47ae6fbfe1dd2f1dbc263485da6fbbe8c051c985
srinivasanr <srinivasan.ramalingam@imaginea.com>
2020-04-12 20:31:40 +0530

    added news_site_scrapy

---
+++ src/ssp/news_scrape/news_scrape/middlewares/common.py
@@ -0,0 +1,23 @@
+'''
+Created on 09-Apr-2020
+
+@author: srinivasan
+'''
+from scrapy.downloadermiddlewares.cookies import CookiesMiddleware
+
+
+class CustomCookiesMiddleware(CookiesMiddleware):
+
+    def process_request(self, request, spider):
+        if 'dont_merge_cookies' in request.meta:
+            return
+        cookiejarkey = request.meta.get("cookiejar")
+        jar = self.jars[cookiejarkey]
+        jar.clear()  # custom line
+        cookies = self._get_request_cookies(jar, request)
+        for cookie in cookies:
+            jar.set_cookie_if_ok(cookie, request)
+        request.headers.pop('Cookie', None)
+        jar.add_cookie_header(request)
+
+        self._debug_cookie(request, spider)
---
+++ src/ssp/news_scrape/news_scrape/utils/__init__.py
@@ -0,0 +1,2 @@
+from news_scrape.utils.user_agents import AGENTS_ALL
+AGENTS = AGENTS_ALL
